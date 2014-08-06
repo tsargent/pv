ed986ebd13c4009bace034bb973dada7043d287d
Tyler Sargent <tsargent@gmail.com>
2014-08-05 22:40:29 -0400

    commit

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,7 +4,7 @@ class PostsController < ApplicationController
   # GET /posts
   # GET /posts.json
   def index
-    @posts = Post.order('created_at DESC')
+    @posts = Post.published.order('created_at DESC')
   end
 
   # GET /posts/1
