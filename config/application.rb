0c8bd169dbae62d9dfdd23081f62b01ce214a288
Tyler Sargent <tsargent@gmail.com>
2015-03-23 21:05:30 -0400

    update postmark config

--- config/application.rb
+++ config/application.rb
@@ -21,8 +21,7 @@ module Pv2
     # config.i18n.default_locale = :de
 
     config.action_mailer.delivery_method = :postmark
-    config.action_mailer.postmark_settings = { :api_key => ENV['POSTMARK_API_KEY'] }
-
+    config.action_mailer.postmark_settings = { :api_token => ENV['POSTMARK_API_TOKEN'] }
 
 
   end
