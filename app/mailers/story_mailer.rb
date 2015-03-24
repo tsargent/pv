97a70fe5d39543232c106c95b858e2e1c557878a
Tyler Sargent <tsargent@gmail.com>
2015-03-24 08:42:53 -0400

    fixing mailer

--- app/mailers/story_mailer.rb
+++ app/mailers/story_mailer.rb
@@ -1,36 +1,14 @@
 class StoryMailer < ActionMailer::Base
-  # default from: "info@projectvoice.org"
-
-
-
-
-
+  default from: "info@projectvoice.org"
 
   def new_submission(story)
-  	@story = story
-
-    mail(
-      :subject => 'Hello from Postmark',
-      :to  => 'tsargent@gmail.com',
-      :from => 'tyler@tylertylertyler.com',
-      :html_body => '<strong>Hello</strong> dear Postmark user. from new_submission',
-      :track_opens => 'true')
-
-
+    @story = story
+    mail(to: 'info@projectvoice.org', subject: 'New Story Submitted')
   end
 
   def thank_you(story)
-  	@story = story
-
-    mail(
-      :subject => 'Hello from Postmark',
-      :to  => 'tsargent@gmail.com',
-      :from => 'tyler@tylertylertyler.com',
-      :html_body => '<strong>Hello</strong> dear Postmark user. from thank_you',
-      :track_opens => 'true')
-
-
+    @story = story
+    mail(to: story.email, subject: 'Thank you for your story')
   end
-
 
 end
